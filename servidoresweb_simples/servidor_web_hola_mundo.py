2895b04514d28820c4a6c4187f72bdfb28d6ad59
Carlos <carlosandresgarcia1986@hotmail.com>
2023-07-20 00:19:53 +0200

    servidores web simples

---
+++ servidoresweb_simples/servidor_web_hola_mundo.py
@@ -0,0 +1,19 @@
+import http.server
+import socketserver
+
+PORT = 
+
+
+class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(bytes("Hello, World!", "utf8"))
+        return
+
+Handler = MyHttpRequestHandler
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("serving at port", PORT)
+    httpd.serve_forever()
